0766d9898b2c1eb327337f546bd9d768ce63bb4d
vpmorais25 <47580635+vpmorais25@users.noreply.github.com>
2019-10-22 00:13:24 -0300

    Add files via upload

---
+++ Fibonacci2/Form1.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Tb1 = New System.Windows.Forms.TextBox()
+        Me.Bstart = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Tb1
+        '
+        Me.Tb1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Tb1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Tb1.Location = New System.Drawing.Point(13, 13)
+        Me.Tb1.Multiline = True
+        Me.Tb1.Name = "Tb1"
+        Me.Tb1.Size = New System.Drawing.Size(548, 331)
+        Me.Tb1.TabIndex = 0
+        '
+        'Bstart
+        '
+        Me.Bstart.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Bstart.Location = New System.Drawing.Point(13, 351)
+        Me.Bstart.Name = "Bstart"
+        Me.Bstart.Size = New System.Drawing.Size(548, 55)
+        Me.Bstart.TabIndex = 1
+        Me.Bstart.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(573, 418)
+        Me.Controls.Add(Me.Bstart)
+        Me.Controls.Add(Me.Tb1)
+        Me.MinimumSize = New System.Drawing.Size(589, 457)
+        Me.Name = "Form1"
+        Me.Text = "Fibonacci 2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Tb1 As TextBox
+    Friend WithEvents Bstart As Button
+End Class
---
+++ Fibonacci2/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+
+    Dim a As Long
+    Dim b As Long
+    Dim c As Long
+    Dim TXT As String
+
+
+    Private Sub Bstart_Click(sender As Object, e As EventArgs) Handles Bstart.Click
+        Fib()
+    End Sub
+
+
+    Private Sub Fib()
+
+        If a = 0 Then
+            Texto(a)
+            b = a
+            a = 1
+        ElseIf a > 0 Then
+            Texto(" - " & a.ToString("#,###"))
+            c = a
+            a = a + b
+            b = c
+
+        End If
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        a = 0
+    End Sub
+
+    Private Sub Texto(txt)
+        Tb1.Text = Tb1.Text & txt
+    End Sub
+End Class
